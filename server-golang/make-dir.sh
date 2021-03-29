24ab012a3e964f0d9f31f3fad3b22392f36ea106
xz1220 <3024979463@qq.com>
2021-03-29 19:30:56 +0800

    Doc: update the doc

--- server-golang/make-dir.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir -p /home/xingzheng/labelprojectdata/image/
-mkdir -p /home/xingzheng/labelprojectdata/images/
-mkdir -p /home/xingzheng/labelprojectdata/imagel/
-mkdir -p /home/xingzheng/labelprojectdata/imaged/
-mkdir -p /home/xingzheng/labelprojectdata/video/
-mkdir -p /home/xingzheng/labelprojectdata/videod/
-mkdir -p /home/xingzheng/labelprojectdata/videos/
-
